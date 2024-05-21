0860314deca6fc385bb007917546d028b972e897
savelmtr <56698459+savelmtr@users.noreply.github.com>
2024-05-21 09:06:14 +0300

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -20,7 +20,7 @@ apt-get install -y docker-ce docker-ce-cli containerd.io
 
 docker run hello-world
 
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-linux-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 chattr +i /usr/local/bin/docker-compose
 
